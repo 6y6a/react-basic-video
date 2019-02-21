e56a79615d97bae3446e2c29a1a7082db5585329
6y6a <a.gubin@leningrad.io>
2019-02-21 23:09:57 +0300

    добавлен SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,32 @@
 import React from 'react'
 
 class SearchBar extends React.Component {
+    state={ term: '' }
+
+    onInputChange = e => {
+        this.setState({term: e.target.value})
+    }
+
+    onFormSubmit = e => {
+        e.preventDefault();
+
+    }
+
     render() {
-        return <div>SearchBar</div>
+        return (
+            <div className='ui segment search-bar'>
+                <form className='ui form' onSubmit={this.onFormSubmit}>
+                    <div className="field">
+                        <label htmlFor="">Video Search</label>
+                        <input
+                            type="text"
+                            value={this.state.term}
+                            onChange={this.onInputChange}
+                        />
+                    </div>
+                </form>
+            </div>
+        )
     }
 }
 
