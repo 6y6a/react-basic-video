a3aa6c5a4cf65ac93a27d8c30109119c9252eb61
6y6a <a.gubin@leningrad.io>
2019-02-21 23:31:29 +0300

    добавлен youtube и axios

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from 'axios'
+
 const KEY = 'AIzaSyCpPVqag9ZAA0vSTr8Uv-tqyH56-RF4ETo';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3/',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY,
+    }
+})
